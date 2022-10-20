11f3df69541b86e6ea78f44615da8cb7cde34b82
Benoit Martel <martel.b@gmail.com>
2022-10-20 15:52:45 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# exploramer
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+## Démarrer exploramer-server avant // yarn serve dans bash
+
+## Le serveur est sur localhost:3030
---
+++ components/Info.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="info">
+    <div class="info popup" @click="done()">
+      <div>
+        <div class="close" @click="done()">X</div>
+        {{ info[lang] }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      lang: this.$store.state.settings.langue,
+    }
+  },
+  props: ['info'],
+  methods: {
+    done() {
+      this.$parent.showInfo = false
+    },
+  },
+}
+</script>
+
+<style></style>
